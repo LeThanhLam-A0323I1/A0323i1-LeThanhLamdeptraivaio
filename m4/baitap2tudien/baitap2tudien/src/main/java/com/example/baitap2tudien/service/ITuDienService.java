88fdf7aa8c865b9a774f5bf048af24b8caf26d0f
LeThanhLam-A0323I1 <129520477+LeThanhLam-A0323I1@users.noreply.github.com>
2024-04-22 19:02:06 +0700

    Delete m4/baitap2tudien/baitap2tudien directory

--- m4/baitap2tudien/baitap2tudien/src/main/java/com/example/baitap2tudien/service/ITuDienService.java
+++
@@ -1,9 +0,0 @@
-package com.example.baitap2tudien.service;
-
-import com.example.baitap2tudien.model.TuDien;
-
-import java.util.List;
-
-public interface ITuDienService {
-    List<TuDien> findAll();
-}
